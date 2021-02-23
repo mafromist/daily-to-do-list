16888ff8587f140fb54f23280e8b000b15acc5ae
Muge <mafromist@gmail.com>
2021-02-23 09:53:29 +0000

    Added localStorage codes

--- src/App.js
+++ src/App.js
@@ -1,91 +1,175 @@
-import {useEffect, useState} from 'react'
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
+const useStateWithLocalStorage = (localStorageKey) => {
+  const[value, setValue] = useState (
+    localStorage.getItem(localStorageKey || "")
+  )
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, value);
+  }, [value]);
+
+  return [value, setValue];
+}
 
 const App = () => {
-const [taskText, setTaskText] = useState('');
-const [taskDeadline, setTaskDeadline] = useState('');
-const [tasks, setTasks] = useState(JSON.parse(localStorage.get('tasks')) || []);
+  const [addTask, setAddTask] = useStateWithLocalStorage('myTaskList');
+  const [addDeadline, setAddDeadline] = useStateWithLocalStorage('myTaskList')
+  const [newTasks, setNewTasks] = useStateWithLocalStorage(JSON.parse('myTaskList') || []);
 
-const handleTextChange = (e) => setTaskText(e.target.value);
-const handleDeadlineChange = (e) => setTaskDeadline(e.target.value);
+  useEffect( () => {
+    localStorage.setItem('myTaskList', addTask);
+  }, [addTask])
 
-const handleClickAddBtn = (e) => {
-  tasks.concat[{handleTextChange}, {handleDeadlineChange}]
-  setTasks(e.target.value)
-}
+  const handleTaskAdd = (e) => {
+    setAddTask(e.target.value);
+  };
+  const handleTaskDeadline = (e) => {
+    setAddDeadline(e.target.value);
+  };
 
-/* class App extends Component {
- /*added a digital date and clock 
- componentDidMount() { 
-  this.update = setInterval(() => {
-      this.setState({ time: new Date() });
-  }, 1 * 1000);
-}
+  const createTask = () => {
+    setNewTasks([
+      ...newTasks,
+      {
+        id: newTasks.length,
+        taskText: addTask,
+        taskDeadline: addDeadline,
+        completed: false,
+      },
+    ]);
+  };
 
-componentWillUnmount() { 
-  clearInterval(this.update);
-}
+  const handleListTasks = (e) => {
+    e.preventDefault();
+    if (addTask === "") return;
+    createTask();
+    setAddTask("");
+    setAddDeadline("");
+  };
 
-} */
+  const handleDeleteTask = (taskId) => {
+    const deletedTasks = newTasks.filter((addTask) => addTask.id !== taskId);
+    setNewTasks(deletedTasks);
+  };
+
+  const [doneTasks, setDoneTasks] = useStateWithLocalStorage(JSON.parse('myTaskList') || []);
+
+  const completeTasks = () => {
+    setDoneTasks([
+      ...doneTasks,
+      {
+        completed: true,
+      },
+    ]);
+  };
+
+  const handleCompletedTasks = (e) => {completeTasks();};
+
+  const deleteCompletedTasks = (taskId) => {
+    const restDoneTasks = doneTasks.filter((doneTask) => doneTask.id !== taskId);
+    setDoneTasks(restDoneTasks);
+  };
 
   return (
     <div className="App">
-        <div className="upCont">
-          <div className="appCont">
-
-            <div className="titleArea">
-              <h1>Like every other to-do-list</h1>
-              <p>Add task and the deadline for them</p>
-            </div>
-            
-            <div className="appInfo">
-              <div className="inputArea">
-                <div>
-                  
-                    {/* user enter the task here */}
-                  <label for="task">Your Task:</label>
-                  <input className="taskInput input" type="text" 
-                  onChange={handleTextChange} placeHolder="Add your Task here"
-                  value={taskText} name="task"></input>
-
-                  </div>
-                  <div>
-
-                  <label for="deadline">When should you finish the task? </label>
-                  <input className="deadlineInput input" name="deadline" 
-                  type="date" value="2018-07-22" onChange={handleDeadlineChange} 
-                  value={taskDeadline}></input>
-
-                </div>
-              </div>
-              <div>
-                  <button className="saveBtn" onClick={handleClickAddBtn}>
-                    Add to List</button>
-              </div>
-            </div>
+      <div className="upCont">
+        <div className="appCont">
+          <div className="titleArea">
+            <h1>Like every other to-do-list</h1>
+            <p>Add task and the deadline for them</p>
           </div>
 
-          <div className="taskArea">
-            <div className="addedTasksArea">
-              <h1>Your Tasks</h1>
-              <div className="addedTaskArea">
-                <ul className="addedTask">{tasks}</ul>
+          <div className="appInfo">
+            <div className="inputArea">
+              <div>
+                {/* user enter the task here */}
+                <label htmlFor="task">Your Task:</label>
+                <input
+                  className="taskInput input"
+                  type="text"
+                  onChange={handleTaskAdd}
+                  placeholder="Add your Task here"
+                  value={addTask}
+                  name="task"
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="deadline">
+                  When should you finish the task?{" "}
+                </label>
+                <input
+                  className="deadlineInput input"
+                  name="deadline"
+                  type="date"
+                  onChange={handleTaskDeadline}
+                  value={addDeadline}
+                ></input>
               </div>
             </div>
-
-            {/* <div className="finishedArea">
-                <h2>finished Tasks</h2>
-                <ul className="finishedTasks">{eachArcTasks}</ul>
-            </div> */}
+            <div>
+              <button className="saveBtn" onClick={handleListTasks}>
+                Add to List
+              </button>
+            </div>
           </div>
         </div>
 
-        {/* <div className="clock">
-            {time.toLocaleString()}
-        </div> */}
+        <div className="taskArea">
+          <div className="addedTasksArea">
+            <h1>Your Tasks</h1>
+            <div className="addedTaskArea">
+              <ul className="addedTask">
+                {newTasks.map((task) => (
+                  <li key={task.id}>
+                    <h2 className="listedTask">
+                      {task.taskText} Deadline:
+                      <span className="listedTaskDate">
+                        {task.taskDeadline}
+                      </span>
+                    </h2>
+                    <button
+                      className="doneBtn"
+                      onClick={() => handleCompletedTasks(task.taskStatus)}
+                    >
+                      Done
+                    </button>
+                    <button
+                      className="deleteBtn"
+                      onClick={() => handleDeleteTask(task.id)}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="finishedArea">
+            <h2>finished Tasks</h2>
+            <ul className="finishedTasks">
+              {doneTasks.map((task) => (
+                <li key={task.id}>
+                  <h2 className="listedTask">
+                    {task.taskText} Deadline:
+                    <span className="listedTaskDate">{task.taskDeadline}</span>
+                  </h2>
+                  <button
+                    className="deleteBtn"
+                    onClick={() => deleteCompletedTasks(task.id)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
+    </div>
   );
-}
+};
 
 export default App;
